b6964a2b6950010779d1bc211a659c32cfe67497
Aishwarya SVS <satyavoluaishwarya@gmail.com>
2023-10-21 01:22:48 -0400

    imported necessary libraries, loading and using the environment variables from .env file

---
+++ Task-1/main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import snowflake.connector
+import pandas as pd
+import plotly.graph_objects as go
+import dotenv
+
+# Use the loaded environment variables
+conn_params = {
+    'account': os.environ.get("SNOWFLAKE_ACCOUNT"),
+    'user': os.environ.get("SNOWFLAKE_USER"),
+    'password': os.environ.get("SNOWFLAKE_PASSWORD"),
+    'warehouse': os.environ.get("SNOWFLAKE_WAREHOUSE"),
+    'database': os.environ.get("SNOWFLAKE_DATABASE"),
+    'schema': os.environ.get("SNOWFLAKE_SCHEMA"),
+}
+
